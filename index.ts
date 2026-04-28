b79fb1e0e6d19012e2a91dc756646f90fbbc400e
latifiss <latifissaka069@gmail.com>
2026-04-28 14:21:27 +0000

    feat: :lipstick: added favicon

--- index.ts
+++ index.ts
@@ -1,12 +1,21 @@
 import 'dotenv/config';
 import mongoose from 'mongoose';
 import http from 'http';
+import path from 'path';
+import express from 'express';
 import { connectDB } from './db/index';
 import { initRealtimeWebsocketServer } from './lib/realtime/ws';
 import { createApp, allowedOrigins } from './app';
 
 export const app = createApp();
 export const server = http.createServer(app);
+
+const publicPath = path.join(__dirname, '..', 'public');
+app.use(express.static(publicPath));
+
+app.get('/favicon.ico', (req, res) => {
+  res.sendFile(path.join(publicPath, 'favicon.ico'));
+});
 
 const PORT = process.env.PORT || 6060;
 
